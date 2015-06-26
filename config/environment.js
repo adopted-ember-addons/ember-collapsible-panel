4671f8f9b4c5b63961eaf54434bd6635dbdc8488
Sam Selikoff <sam.selikoff@gmail.com>
2015-06-26 15:21:37 -0400

    cleaner api for checking for liquidfire

--- config/environment.js
+++ config/environment.js
@@ -6,17 +6,9 @@ var VersionChecker = require('ember-cli-version-checker');
 module.exports = function( environment, appConfig, addon ) {
   appConfig['ember-collapsible-panel'] = appConfig['ember-collapsible-panel'] || {};
 
-    var checker = new VersionChecker(addon);
-    var dep = checker.for('liquid-fire', 'npm');
-    var hasLiquidFire = false;
-
-    try {
-      if (dep.gt('0.17.0')) {
-        hasLiquidFire = true;
-      }
-    } catch(e) {
-
-    }
+  var checker = new VersionChecker(addon);
+  var dep = checker.for('liquid-fire', 'npm');
+  var hasLiquidFire = dep.version;
 
   appConfig['ember-collapsible-panel']['hasLiquidFire'] = hasLiquidFire;
 
