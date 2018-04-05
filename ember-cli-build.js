f9e215722effe99c4040cfc6608d4595c1e3f094
Alon Bukai <alonzorz@gmail.com>
2018-04-05 19:16:16 +0300

    Added Bootstrap

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -17,7 +17,7 @@ module.exports = function(defaults) {
     This build file does *not* influence how the addon or the app using it
     behave. You most likely want to be modifying `./index.js` or app's build file
   */
-  app.import(app.bowerDirectory + '/bootstrap/dist/css/bootstrap.css');
+  app.import('node_modules/bootstrap/dist/css/bootstrap.css');
 
   return app.toTree();
 };
