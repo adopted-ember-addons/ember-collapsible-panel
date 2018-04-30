127f7b7d7f0cbe56077faac823fb7c1bb85d6ddd
Sam Selikoff <sam.selikoff@gmail.com>
2018-04-30 07:39:00 -0700

    Ensure app directory only re-exports addon code

--- app/services/dependency-checker.js
+++ app/services/dependency-checker.js
@@ -1,11 +1,1 @@
-import { computed } from '@ember/object';
-import Service from '@ember/service';
-import ENV from '../config/environment';
-
-export default Service.extend({
-
-  hasLiquidFire: computed('', function() {
-    return ENV['ember-collapsible-panel'].hasLiquidFire;
-  })
-
-});
+export { default } from 'ember-collapsible-panel/services/dependency-checker';
