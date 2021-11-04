597e1bbb0f29bfaf07ba1c56e8cf7efc94e75e11
Travis Hoover <thoov7@gmail.com>
2021-11-04 13:25:01 -0700

    Fix usage under Embroider
    liquid-if was unresolvable because liquid-fire was a devDep and
    not a dependecies of this addon.
    
    Co-Authored-By: Robert Jackson <me@rwjblue.com>

--- addon/components/cp-panel-body/component.js
+++ addon/components/cp-panel-body/component.js
@@ -1,10 +1,27 @@
+import { macroCondition, dependencySatisfies, importSync } from '@embroider/macros';
+import { ensureSafeComponent } from '@embroider/util';
 import Component from '@ember/component';
+import { computed } from '@ember/object';
+import { assert } from '@ember/debug';
 import layout from './template';
+
+let LiquidIf, hasRealLiquidIf;
+if (macroCondition(dependencySatisfies('liquid-fire', '*'))) {
+  LiquidIf = importSync('liquid-fire/components/liquid-if').default;
+  hasRealLiquidIf = true;
+} else {
+  hasRealLiquidIf = false;
+}
 
 export default Component.extend({
   layout,
 
   classNames: ['cp-Panel-body'],
-  classNameBindings: ['isOpen:cp-is-open']
+  classNameBindings: ['isOpen:cp-is-open'],
 
+  // ideally we'd use a getter here, but that requires Ember 3.16+
+  'liquidif': computed(function() {
+    assert('ember-collapsible-panel\'s cp-panel-body component requires liquid-fire to be present if `shouldAnimate` is used', hasRealLiquidIf);
+    return ensureSafeComponent(LiquidIf, this);
+  }),
 });
