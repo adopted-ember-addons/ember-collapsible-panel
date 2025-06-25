ebb2a4a70dce72e6f6d6611b806774c2efe7d87b
Alex Kanunnikov <lifeart92@gmail.com>
2025-06-25 20:00:51 +0300

    fix action deprecation

--- tests/dummy/app/pods/demo/programmatic-control/component.js
+++ tests/dummy/app/pods/demo/programmatic-control/component.js
@@ -1,37 +1,34 @@
 /* eslint-disable ember/no-actions-hash, ember/no-classic-classes, ember/no-classic-components, ember/no-get, ember/require-tagless-components, prettier/prettier */
 import { inject as service } from '@ember/service';
 import Component from '@ember/component';
+import { action } from '@ember/object';
 
 // BEGIN-SNIPPET programmatic-control
 export default Component.extend({
-
   panelActions: service(),
 
-  actions: {
-    expandAll() {
-      this.get('panelActions').openAll('group1');
-    },
+  expandAll: action(function () {
+    this.get('panelActions').openAll('group1');
+  }),
 
-    collapseAll() {
-      this.get('panelActions').closeAll('group1');
-    },
+  collapseAll: action(function () {
+    this.get('panelActions').closeAll('group1');
+  }),
 
-    togglePanelA() {
-      this.get('panelActions').toggle('panelA');
-    },
+  togglePanelA: action(function () {
+    this.get('panelActions').toggle('panelA');
+  }),
 
-    togglePanelB() {
-      this.get('panelActions').toggle('panelB');
-    },
+  togglePanelB: action(function () {
+    this.get('panelActions').toggle('panelB');
+  }),
 
-    openPanelA() {
-      this.get('panelActions').open('panelA');
-    },
+  openPanelA: action(function () {
+    this.get('panelActions').open('panelA');
+  }),
 
-    closePanelA() {
-      this.get('panelActions').close('panelA');
-    }
-  }
-
+  closePanelA: action(function () {
+    this.get('panelActions').close('panelA');
+  }),
 });
 // END-SNIPPET
